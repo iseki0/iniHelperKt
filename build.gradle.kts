f5ac8c823f96341ada782df5a9be6ba6d5724aa0
iseki <admin@iseki.space>
2020-03-13 16:29:14 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
-
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath ("ru.vyarus:gradle-pom-plugin:2.1.0")
+    }
+}
 
 plugins {
     kotlin("jvm") version "1.3.70"
+    `maven-publish`
+    id("ru.vyarus.pom") version "2.1.0"
+    maven
 }
 
 group = "space.iseki"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -21,5 +31,27 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+
+publishing {
+    repositories {
+        maven {
+            name = "iniHelperKt"
+            // change to point to your repo, e.g. http://my.org/repo
+            url = uri("https://maven.pkg.github.com/cpdyj/iniHelperKt")
+            credentials {
+                username = System.getenv("GithubUserId")
+                password = System.getenv("GithubAPIKey")
+            }
+        }
+
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            artifact("./build/libs/${rootProject.name}-$version.jar")
+
+        }
     }
 }
